2da204d6fd845281c6ffe4fb14a1362328550305
Nela Michálková <nela.chrapkova@seznam.cz>
2026-07-06 20:45:53 +0200

    práce na numerickém modelu

--- glitchrises-master/generate_tex.py
+++ glitchrises-master/generate_tex.py
@@ -4,9 +4,26 @@ import re
 df_res_1 = pd.read_csv("Experiment_1_Tabulka_Rcci.csv")
 df_res_2 = pd.read_csv("Experiment_2_Tabulka_Mcoeff.csv")
 
+# Transform table from Experiment 1: drop 'Kolo' and rename 'M_coeff' to 'M_core'
+if "Kolo" in df_res_1.columns:
+    df_res_1 = df_res_1.drop(columns=["Kolo"])
+if "M_coeff" in df_res_1.columns:
+    df_res_1 = df_res_1.rename(columns={"M_coeff": "M_core"})
+
+df_res_1.to_csv("Experiment_1_Tabulka_Rcci.csv", index=False)
+
+# Transform table from Experiment 2: drop 'Kolo' and rename 'M_coeff' to 'M_core'
+if "Kolo" in df_res_2.columns:
+    df_res_2 = df_res_2.drop(columns=["Kolo"])
+if "M_coeff" in df_res_2.columns:
+    df_res_2 = df_res_2.rename(columns={"M_coeff": "M_core"})
+
+df_res_2.to_csv("Experiment_2_Tabulka_Mcoeff.csv", index=False)
+
 rename_cols = {
     "R_cci [km]": "$R_{\\rm cci}$ [km]",
     "M_coeff": "$M_{\\rm coeff}$",
+    "M_core": "$M_{\\rm core}$",
     "M_NS [M_sun]": "$M_{\\rm NS}$ [$M_{\\odot}$]",
     "R_NS [km]": "$R_{\\rm NS}$ [km]",
     "Max Profil A [muHz]": "Max Profil A [$\\mu$Hz]",
@@ -28,3 +45,4 @@ def export_latex(df, filename):
 export_latex(df_res_1, "Experiment_1_Tabulka_Rcci.tex")
 export_latex(df_res_2, "Experiment_2_Tabulka_Mcoeff.tex")
 print("TeX tables generated.")
+
